dcd156f2cbe7b5ca118227b2060c5b6585c1e3d8
inhouuu <fabiobrasil2002@hotmail.com>
2023-03-09 15:28:42 -0300

    teste commit

--- pages/dynamic/[id].tsx
+++ pages/dynamic/[id].tsx
@@ -6,8 +6,25 @@ import Header from '@/components/header'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+export async function getStaticProps(context:any){
+    const {params} = context;
+    const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${params.todoId}`);
+    const todo = await data.json();
+    // console.log('API FAKE TEST:', todo);
+    return{
+       props:{todo},
+    }
+}
 
-export default function Home() {
+export async function getStaticPaths() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+    const data = await response.json();
+    console.log(data)
+    const paths = data.map((todo:any) => ({ params: { id: String(todo.id) } }));
+    return { paths, fallback: false }
+}
+
+export default function Todo({todo}) {
     const router = useRouter();
     const id = router.query.id;
 
@@ -16,6 +33,7 @@ export default function Home() {
             <main className={styles.main}>
                 <Link href="/dynamic" legacyBehavior><a>Voltar</a></Link>
                 <h3>Exibindo id page dynamic: {id}</h3>
+                <h3>Exibindo id todo: {todo.id}</h3>
             </main>
         </>
     )
